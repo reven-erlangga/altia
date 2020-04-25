37a1fe3601b6655890d420195bde3ea8047816d2
Reven Ferlian Erlangga <reven.erlangga@gmail.com>
2020-04-26 02:37:47 +0700

    Install package & auth
    Package
    - Laravel UI by taylorotwell
    - ui bootstrap
    
    Auth
    - Register & Login

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('admin.templates.default');
+    return view('welcome');
 });
 
-Route::get('user', function () {
-    return view('admin.user.index');
-});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
