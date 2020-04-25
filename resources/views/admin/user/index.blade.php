4d84bbe2b5b22162f415bf08768254d29aa6d2bb
Reven Ferlian Erlangga <reven.erlangga@gmail.com>
2020-04-26 01:11:14 +0700

    Template admin lte
    Pembuatan master child view

---
+++ resources/views/admin/user/index.blade.php
@@ -0,0 +1,5 @@
+@extends('admin.templates.default')
+
+@section('content')
+    <h1>User</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('admin.templates.default');
 });
+
+Route::get('user', function () {
+    return view('admin.user.index');
+});
