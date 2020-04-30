8741cc76e2c9abe3408747be0289b490a2a8d65c
Reven Ferlian Erlangga <reven.erlangga@gmail.com>
2020-04-30 19:44:24 +0700

    Pembuatan controller author dan view
    1. Pembuatan author controller di dalam folder admin
    2. Pemberian route untuk akses author controller

---
+++ resources/views/admin/author/index.blade.php
@@ -0,0 +1,5 @@
+@extends('admin.templates.default')
+
+@section('content')
+<h1>Penulis</h1>
+@endsection
--- routes/admin.php
+++ routes/admin.php
@@ -1,3 +1,4 @@
 <?php
 
 Route::get('/', 'HomeController@index')->name('dashboard');
+Route::get('/author', 'AuthorController@index')->name('author.index');
