5a3d3010ab2f6b028825dc764a71cb2dde75d1bf
Reven Ferlian Erlangga <reven.erlangga@gmail.com>
2020-04-30 08:08:27 +0700

    Revert "Role"
    This reverts commit f595aeb63d566af2bd594a2c8160ecb020a584e9.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,14 +64,10 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        $user = User::create([
+        return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
-
-        $user->assignRole('user');
-
-        return $user;
     }
 }
