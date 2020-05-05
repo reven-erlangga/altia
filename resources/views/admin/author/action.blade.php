a2ae239783b398d1963b0415770c167dd69f1f6f
Reven Ferlian Erlangga <reven.erlangga@gmail.com>
2020-05-05 16:38:08 +0700

    Konfirmasi dialog hapus data
    Konfirmasi dialog hapus data dengan sweetalert2 (CDN)

--- resources/views/admin/author/action.blade.php
+++ resources/views/admin/author/action.blade.php
@@ -1,13 +1,33 @@
 <a href="{{ route('admin.author.edit', $model) }}" class="btn btn-warning">Edit</a>
 <button href="{{ route('admin.author.destroy', $model) }}" id="delete" class="btn btn-danger">Hapus</button>
 
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
 <script>
     $('button#delete').on('click', function(e) {
         e.preventDefault();
 
         var action = $(this).attr('href');
 
-        document.getElementById('deleteForm').action = action;
-        document.getElementById('deleteForm').submit();
+        Swal.fire({
+            title: 'Apakah kamu yakin hapus data ini ?',
+            text: "Data yang sudah dihapus tidak dapat dikembalikan!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, hapus data!'
+            }).then((result) => {
+            if (result.value) {
+                document.getElementById('deleteForm').action = action;
+                document.getElementById('deleteForm').submit();
+
+                Swal.fire(
+                'Terhapus!',
+                'Data dihapus.',
+                'success'
+                )
+            }
+            })
+
     })
 </script>
