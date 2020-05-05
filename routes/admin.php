c3cafc05186cf0db8a85c99298d733c8d17fe866
Reven Ferlian Erlangga <reven.erlangga@gmail.com>
2020-05-05 19:06:45 +0700

    Penyederhanaan route
    Menyederhanakan route pada author controller (AuthorController)

--- routes/admin.php
+++ routes/admin.php
@@ -2,11 +2,5 @@
 
 Route::get('/', 'HomeController@index')->name('dashboard');
 
-Route::get('/author', 'AuthorController@index')->name('author.index');
-Route::get('/author/create', 'AuthorController@create')->name('author.create');
-Route::post('/author', 'AuthorController@store')->name('author.store');
-Route::get('/author/{author}/edit', 'AuthorController@edit')->name('author.edit');
-Route::put('/author/{author}', 'AuthorController@update')->name('author.update');
-Route::delete('/author/{author}', 'AuthorController@destroy')->name('author.destroy');
-
 Route::get('/author/data','DataController@authors')->name('author.data');
+Route::resource('author', 'AuthorController');
