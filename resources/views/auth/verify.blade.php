b2749853655381a59f4a88d5d82bbb0bc8015a9f
Reven Ferlian Erlangga <reven.erlangga@gmail.com>
2020-05-08 08:01:57 +0700

    Implementasi materialize ke tampilan verifikasi
    Mengubah tampilan bootstrap ke materialize

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,24 +1,22 @@
-@extends('layouts.app')
+@extends('frontend.template.default')
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
-
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
-                    @endif
-
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+    <div class="col s12 m7">
+        <h4 class="header">{{ __('Verify Your Email Address') }}</h4>
+        <div class="card horizontal">
+            <div class="card-stacked">
+                <div class="card-content" role="alert">
+                @if (session('resent'))
+                    <p>{{ __('A fresh verification link has been sent to your email address.') }}</p><br>
+                @endif
+                    <p>{{ __('Before proceeding, please check your email for a verification link.') }}</p>
+                    <p>{{ __('If you did not receive the email') }},</p>
+                </div>
+                <div class="card-action">
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                        @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                    @csrf
+                    <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>
                     </form>
                 </div>
             </div>
