5b68b7db1296f34cc73b4fc1242f63d680218692
Reven Ferlian Erlangga <reven.erlangga@gmail.com>
2020-05-08 18:44:15 +0700

    Membenahi navigasi relasi user dan buku

--- resources/views/frontend/template/partials/navigation.blade.php
+++ resources/views/frontend/template/partials/navigation.blade.php
@@ -6,14 +6,46 @@
             <a href="{{ route('homepage') }}" class="brand-logo">Perpusku</a>
             <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
             <ul class="right hide-on-med-and-down">
+                @guest
                 <li><a href="{{ route('login') }}">Login</a></li>
                 <li><a href="{{ route('register') }}">Register</a></li>
+                @else
+                <ul id="dropdown1" class="dropdown-content">
+                    <li><a href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                                      document.getElementById('logout-form').submit();">Logout</a></li>
+                </ul>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+
+                <li>
+                    <a href="" class="dropdown-trigger" data-target="dropdown1">{{ auth()->user()->name }}</a>
+                </li>
+                @endguest
             </ul>
         </div>
     </nav>
 </div>
 
     <ul class="sidenav" id="mobile-demo">
-        <li><a href="{{ route('login') }}">Login</a></li>
-        <li><a href="{{ route('register') }}">Register</a></li>
+        @guest
+                <li><a href="{{ route('login') }}">Login</a></li>
+                <li><a href="{{ route('register') }}">Register</a></li>
+                @else
+                <ul id="dropdown1" class="dropdown-content">
+                    <li><a href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                                      document.getElementById('logout-form').submit();">Logout</a></li>
+                </ul>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+
+                <li>
+                    <a href="" class="dropdown-trigger" data-target="dropdown1">{{ auth()->user()->name }}</a>
+                </li>
+                @endguest
     </ul>
